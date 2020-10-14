cbf69fdc733c37383ad02164c8bdb4032dbcabe7
dillon <dillonkellar@protonmail.com>
2020-10-14 08:17:44 -0500

    Added buidler config for buidler-deploy and etherscan

--- buidler.config.js
+++ buidler.config.js
@@ -1,6 +1,38 @@
+require('dotenv').config();
+
+const { InfuraProvider } = require('@ethersproject/providers');
+const { fromPrivateKey } = require('ethereumjs-wallet');
+const { randomBytes } = require('crypto');
+
 usePlugin("buidler-ethers-v5");
+usePlugin("buidler-deploy");
+usePlugin("@nomiclabs/buidler-etherscan");
+
+const keys = {
+  rinkeby: fromPrivateKey(
+    process.env.RINKEBY_PVT_KEY
+      ? Buffer.from(process.env.RINKEBY_PVT_KEY.slice(2), 'hex')
+      : randomBytes(32)).getPrivateKeyString()
+};
+
+// npx buidler verify --constructor-args arguments.js address
 
 module.exports = {
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: new InfuraProvider("rinkeby", process.env.INFURA_PROJECT_ID).connection.url,
+      accounts: [keys.rinkeby],
+      chainId: 4
+    },
+  },
   solc: {
     version: "0.6.8",
     optimizer: {
